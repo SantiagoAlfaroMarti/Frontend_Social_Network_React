ae8b2fb9b934a3610019e4e87e164e84721f1469
unknown <santiagoalfamar@gmail.com>
2024-08-20 23:39:48 +0200

    feat: create Header.jsx and Header.css

---
+++ Frontend Social Network/src/components/Header/Header.jsx
@@ -0,0 +1,80 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { CSurfer } from '../CSurfer/CSufer';
+import './Header.css';
+
+export const Header = () => {
+  const { passport, setPassport } = useAuth();
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    localStorage.removeItem("passport");
+    setPassport(null);
+    navigate("/login");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+      <div className="container">
+        <a
+          className="navbar-brand"
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate('/');
+          }}
+        >
+          <span className="red">SOCIAL</span>NETWORK
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <CSurfer path="/" content="Home" />
+            </li>
+            <li className="nav-item">
+              <CSurfer path="/profile" content="Profile" />
+            </li>
+            <li className="nav-item">
+              <CSurfer path="/createPost" content="CreatePost" />
+            </li>
+            <li className="nav-item">
+              <CSurfer path="/allPosts" content="AllPosts" />
+            </li>
+            {passport && passport.token && passport.tokenData?.role === "admin" && (
+              <li className="nav-item">
+                <CSurfer path="/admin" content="Admin" />
+              </li>
+            )}
+            {passport && passport.token ? (
+              <li className="nav-item">
+                <div className="logout" onClick={logOut}>
+                  Logout
+                </div>
+              </li>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <CSurfer path="/login" content="Login" />
+                </li>
+                <li className="nav-item">
+                  <CSurfer path="/register" content="Register" />
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
