327f94233f1557dbaec7ff251506a2de28cc773e
unknown <santiagoalfamar@gmail.com>
2024-08-21 00:13:47 +0200

    feat: create Body.jsx

---
+++ Frontend Social Network/src/views/Body/Body.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { NotFound } from '../NotFound/NotFound';
+import { Home } from '../Home/Home';
+import { Login } from "../Login/Login";
+import { Register }  from '../Register/Register';
+import { Profile } from '../Profile/Profile';
+import { Admin } from '../Admin/Admin';
+import { CreatePost } from '../CreatePost/CreatePost';
+import { AllPosts } from '../AllPosts/AllPosts';
+
+function Body() {
+  
+  const passport = JSON.parse(localStorage.getItem("passport"));
+    let role = null;
+
+    if (passport) {
+        role = passport.tokenData.role;
+    }
+
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/createPost" element={<CreatePost />} />
+        <Route path="/allPosts" element={<AllPosts />} />
+        {role === "admin" && <Route path="/admin" element={<Admin />} />}
+      </Routes>
+    </>
+  );
+}
+
+export default Body;
