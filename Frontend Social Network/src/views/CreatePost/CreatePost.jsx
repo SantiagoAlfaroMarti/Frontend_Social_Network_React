c344fc77f28811a6be24beac4306d313792da032
unknown <santiagoalfamar@gmail.com>
2024-08-21 22:22:28 +0200

    feat: create CreatePost.jsx and CreatePost.css

---
+++ Frontend Social Network/src/views/CreatePost/CreatePost.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import "./CreatePost.css";
+import { createPost } from '../../services/postApiCalls';
+
+export const CreatePost = () => {
+    const [newPost, setNewPost] = useState({
+        description: ""
+    });
+
+    const { passport } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!passport || !passport.token) {
+            navigate("/login");
+        }
+    }, [passport, navigate]);
+
+    const inputHandler = (e) => {
+        setNewPost({
+            ...newPost,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await createPost(newPost, passport.token);
+            if (response.success) {
+                navigate("/profile");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>CreatePost</h1>
+            <div>
+                <div>
+                    <br />
+                    <h3>Share your daily life with others</h3>
+                    <div>
+                        <textarea
+                            value={newPost.description}
+                            name="description"
+                            placeholder="description"
+                            onChange={(e) => inputHandler(e)}
+                        />
+                        <br />
+                        <input type="button" value="Confirm" onClick={handleSubmit} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
