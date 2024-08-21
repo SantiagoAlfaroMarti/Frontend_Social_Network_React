42599ed54e2178f44f6e37a9283f3e5da148e2d4
unknown <santiagoalfamar@gmail.com>
2024-08-21 22:24:31 +0200

    update: update AllPosts.jsx

--- Frontend Social Network/src/views/AllPosts/AllPosts.jsx
+++ Frontend Social Network/src/views/AllPosts/AllPosts.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
 import { getAllPosts } from '../../services/postApiCalls';
+import { useAuth } from '../../contexts/AuthContext';
 
 export const AllPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+  const { passport } = useAuth();
 
-    const [posts, setPosts] = useState([]);
-
-    const navigate = useNavigate();
-  
-    const { passport } = useAuth();
-  
-    useEffect(() => {
+  useEffect(() => {
+    if (passport && passport.token) {
       const bringAllPosts = async () => {
         try {
-          const response = await getAllPosts();
-          setPosts(response.data);
+          const response = await getAllPosts(passport.token);
+          if (response.success) {
+            setPosts(response.data);
+          } else {
+            console.error("Error fetching posts:", response.message);
+          }
         } catch (error) {
-          console.error("Error fetching services:", error);
+          console.error("Error fetching posts:", error);
         }
       };
-  
+
       bringAllPosts();
-    }, []);
+    }
+  }, [passport]);
 
   return (
-    <div>
-      <h1>Posts</h1>
-      {services.map((posts) => (
-        <div key={posts.id}>
-          <CCard description={posts.description} />
+    <>
+      <h1>All Posts</h1>
+      {posts.length === 0 ? (
+        <p>No posts found...</p>
+      ) : (
+        <div>
+          {posts.map((post) => (
+            <div key={post._id}>
+              <div>{post.description}</div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-);
+      )}
+    </>
+  );
 };
