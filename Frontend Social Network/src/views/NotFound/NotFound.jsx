eafe32c85dc5492ce3b72a99198b2a8363bfdf7c
unknown <santiagoalfamar@gmail.com>
2024-08-26 15:18:43 +0200

    update: update NotFound.jsx

--- Frontend Social Network/src/views/NotFound/NotFound.jsx
+++ Frontend Social Network/src/views/NotFound/NotFound.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
 import "./NotFound.css";
 
 export const NotFound = () => {
   return (
-    <div><h1>NotFound</h1></div>
-  )
-}
+    <div className="notfound-wrapper">
+      <div className="notfound-container">
+        <h1 className="notfound-title">404</h1>
+        <p className="notfound-message">Page Not Found</p>
+        <p className="notfound-description">
+          Oops! It looks like the page you're looking for has been moved or doesn't exist. Please check the URL or go back to the homepage.
+        </p>
+        <a href="/" className="notfound-link">Back to Home</a>
+      </div>
+    </div>
+  );
+};
